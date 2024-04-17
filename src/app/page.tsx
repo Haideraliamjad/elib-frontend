c54e848b17fcf8ca2f33566fb3e05367df8454c3
Haider <haideraliamjad4391@gmail.com>
2024-04-17 10:29:55 -0700

    add banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
+import Banner from "@/components/Banner";
 export default function Home() {
-  return (
-    <h1 className="text-center text-xl cursor-pointer select-none">Welcome to elib app</h1>
-  );
+  return <Banner />;
 }
